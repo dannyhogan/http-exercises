a35cd213c4011cdd8d66c072701ae2ddf7d11059
Dan Hogan <hogan.daniel@hotmail.com>
2019-06-26 14:43:16 -0700

    Completed exercise 2

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-// Exercise #1
+// Exercise #2
 const { createServer } = require('http');
 
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('Hello world!');
+
+  const html = /*html*/`<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>I am the best</h1>
+    <ul>
+      <li>Hello</li>
+      <li>World</li>
+      <li>Test</li>
+    </ul>
+  </body>
+  </html>`
+  res.end(html);
 });
 
 server.listen(7890, () => {
